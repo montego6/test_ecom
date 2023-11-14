f107691e26f317d25816b5631f6afaed784bc5a7
Kirill Kirsanov <montego@yandex.ru>
2023-11-15 00:38:29 +0300

    Added tests

--- main/views.py
+++ main/views.py
@@ -5,35 +5,25 @@ from rest_framework.response import Response
 from .validators import get_data_type
 
 
-db = TinyDB('db.json', cache_size=0)
+db = TinyDB('db.json')
 
 
 class GetFormView(APIView):
     def post(self, request):
         db.clear_cache()
-        for field, value in request.query_params.items():
+        for field, value in request.data.items():
             fragment = {}
             fragment[field] = get_data_type(value)
             queryset = db.search(Query().fragment(fragment))
             for schema in queryset:
                 name = schema.pop('name')            
                 for schema_field, schema_value in schema.items():
-                    if (not schema_field in request.query_params) or (schema_value != get_data_type(request.query_params.get(schema_field))):
+                    if (not schema_field in request.data) or (schema_value != get_data_type(request.data.get(schema_field))):
                         break
                 else: 
                     return Response({'name': name}, status=status.HTTP_200_OK)
 
-        # for schema in db:
-        #     name = schema.pop('name')
-        #     for field, type in schema.items():
-        #         if not field in request.query_params:
-        #             print(field, ' is not in params')
-        #             break
-        #         if not type == get_data_type(request.query_params[field]):
-        #             break
-        #     else:
-        #         return Response({'name': name}, status=status.HTTP_200_OK)
         output = {}
-        for field, value in request.query_params.items():
+        for field, value in request.data.items():
             output[field] = get_data_type(value)
         return Response(output, status=status.HTTP_200_OK)
---
+++ tests/test_view.py
@@ -0,0 +1,16 @@
+import json
+from django.urls import reverse
+import pytest
+from rest_framework.test import APIClient
+
+
+with open('test_requests.json') as file:
+    queries = json.load(file)
+
+client = APIClient()
+
+@pytest.mark.parametrize('query', queries)
+def test_get_form_view(query):
+    response = client.post(reverse('get-form'), query['request'])
+    assert response.data == query["response"]
+    assert response.status_code == 200
